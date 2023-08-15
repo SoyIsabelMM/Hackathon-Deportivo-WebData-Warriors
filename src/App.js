53eeb5e1bdc4debaa5facbcbdedf9a307c798708
Matias Campos <lolo252000@gmail.com>
2023-08-14 21:24:27 -0300

    terminando las secciones de los graficos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import Grafica from "./components/Grafica/Grafica.jsx";
 import Header from "./components/Header";
+import Imagen from './components/Grafica/Imagen.jsx';
+import './components/Grafica/Grafica.css';
 
 function App() {
+  console.log(Imagen);
   return (
     <div className="page">
       <Header />
+      <Grafica descripcion={0} imagen={Imagen} />
+      <Grafica descripcion={1} imagen={Imagen} className='reverse' />
+      <Grafica descripcion={2} imagen={Imagen} />
+      <Grafica descripcion={3} imagen={Imagen} className='reverse' />
     </div>
   );
 }
