a243d5a5b5aeaa909d972f75ccc77fce05c6bc4a
Isa <isa@MacBook-Air-de-Daniel.local>
2023-08-15 11:41:55 -0400

    Edit: Se paso los componentes relacionado a las gráficas al componente Main, dejando App solo para los componentes Header, Main y Footer y dar legibilidad al proyecto

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import FrontPage from "./FrontPage";
 import AboutUs from "./AboutUs";
+import Imagen from "./Grafica/Imagen";
+import Grafica from "./Grafica/Grafica";
+import "./Grafica/Grafica.css";
 
 function Main() {
   return (
     <main className="main">
       <FrontPage />
+      <Grafica descripcion={0} imagen={Imagen} />
+      <Grafica descripcion={1} imagen={Imagen} className="reverse" />
+      <Grafica descripcion={2} imagen={Imagen} />
+      <Grafica descripcion={3} imagen={Imagen} className="reverse" />
       <AboutUs />
     </main>
   );
