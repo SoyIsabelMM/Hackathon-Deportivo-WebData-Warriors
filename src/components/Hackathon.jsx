c9eb60dd442df00bc43dccca0ab8884d830ebcf6
Isa <isa@MacBook-Air-de-Daniel.local>
2023-08-16 18:06:30 -0400

    style: Colocar estilos al componente Archers y homologar los nombres de las clases, se elimino la carpeta Archers y se pasaron los archivos a la carpeta components

--- src/components/Hackathon.jsx
+++ src/components/Hackathon.jsx
@@ -6,14 +6,14 @@ function Hackathon() {
       <section className="hackathon" id="hackathon">
         <div className="divider"></div>
         <h2 className="title">Hackathon Deportivo TripleTen</h2>
-        <p className="hackathon__paragraph">
+        <p className="paragraph">
           El Hackathon Deportivo es una competencia amistosa en la que nos retan
           a realizar un análisis estadístico de los datos de la FIFA y
           presentarlos de forma accesible en línea. Trabajando junto a
           compañeros de Data Science y Desarrollo Web, creamos un sitio web que
           recoge la historia de la FIFA desde 1993 hasta hoy.
         </p>
-        <p className="hackathon__paragraph">
+        <p className="paragraph">
           Este hackathon ha sido una experiencia enriquecedora que nos permitió
           sumergirnos en el trabajo en equipo, tomar decisiones basadas en los
           datos aportados por nuestros colegas en Data Scientist. Con
